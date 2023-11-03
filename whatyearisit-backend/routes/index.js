ff12247b718f35d8936503290e6f24238634933e
idriss <idriss.adinani@gmai.com>
2023-11-03 16:20:32 +0100

    whatyear

--- whatyearisit-backend/routes/index.js
+++ whatyearisit-backend/routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/year', (req, res) => {
-  const today = new Date();
-  console.log(today.getFullYear());
-  res.json({ year : today.getFullYear() });
+  const today = new Date().getFullYear().toString();
+  res.json({ year : today });
 });
 
 module.exports = router;
