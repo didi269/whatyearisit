ac7899563c1afaea951247a29a4075bd7ecef259
idriss <idriss.adinani@gmai.com>
2023-11-03 11:49:36 +0100

    kuhuk

--- whatyearisit-backend/routes/index.js
+++ whatyearisit-backend/routes/index.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
 router.get('/year', (req, res) => {
   const today = new Date();
   console.log(today.getFullYear());
